3405a2c70089b8af7ec3585acabfb5c2f2431a81
suhaasbadada <suhaasbadada@gmail.com>
2024-02-27 11:18:54 +0530

    Navbar, setting up components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme } from './utils/Themes';
+import Navbar from './components/Navbar';
+import Skills from './components/Skills';
+import HeroSection from './components/HeroSection';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const Body=styled.div`
+background-color:${({theme})=>theme.bg};
+width:100%;
+height:100%;
+overflow-x:hidden;
+`
+const Wrapper = styled.div`
+  padding-bottom: 100px;
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      Portfolio
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+      <Navbar/>
+      <Body>
+        <HeroSection/>
+        <Wrapper>
+          <Skills/>
+        </Wrapper>
+      </Body>
+      </Router>
+      </ThemeProvider>
   );
 }
 
